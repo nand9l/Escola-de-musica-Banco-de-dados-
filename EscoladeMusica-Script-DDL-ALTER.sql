87ba50d544781657763a6c590f041384bb41507d
José Fernando <166651961+nand9l@users.noreply.github.com>
2024-10-18 17:55:04 -0300

    todos os inserts (DML)

--- EscoladeMusica-Script-DDL-ALTER.sql
+++ EscoladeMusica-Script-DDL-ALTER.sql
@@ -3,27 +3,23 @@
 ALTER TABLE `mydb`.`orquestra` 
 MODIFY COLUMN `nome` VARCHAR(100);
 
---  Renomear a tabela sinfonia para composicao_sinfonica
-RENAME TABLE `mydb`.`sinfonia` TO `composicao_sinfonica`;
+ALTER TABLE `mydb`.`musico` 
+MODIFY COLUMN `cpf` VARCHAR(100);
 
--- Renomear a coluna descricao na tabela funcao para funcao_descricao
-ALTER TABLE `mydb`.`funcao` 
-CHANGE COLUMN `descricao` `funcao_descricao` VARCHAR(100);
+--  Renomear a tabela sinfonia para composicao_sinfonica
+RENAME TABLE `mydb`.`composicao_sinfonica` TO `sinfonia`;
+
 
 -- Adicionar uma coluna email à tabela musico
 ALTER TABLE `mydb`.`musico` 
 ADD COLUMN `email` VARCHAR(100) AFTER `nome`;
 
 --  Renomear a tabela instrumentos para instrumento_musical
-RENAME TABLE `mydb`.`instrumentos` TO `instrumento_musical`;
+RENAME TABLE `mydb`.`instrumento_musical` TO `instrumento`;
 
 -- Modificar o tipo da coluna nacionalidade na tabela musico para VARCHAR(30)
 ALTER TABLE `mydb`.`musico` 
 MODIFY COLUMN `nacionalidade` VARCHAR(30);
-
---  Remover a coluna data_audicao da tabela musico
-ALTER TABLE `mydb`.`musico` 
-DROP COLUMN `email`;
 
 -- Modificar o tipo da coluna nome na tabela orquestra para VARCHAR(100)
 ALTER TABLE `mydb`.`orquestra` 
@@ -34,17 +30,14 @@ ALTER TABLE `mydb`.`musico`
 ADD COLUMN `telefone` VARCHAR(15) AFTER `email`;
 
 --  Renomear a coluna data_criacao na tabela composicao_sinfonica para data_composta
-ALTER TABLE `mydb`.`composicao_sinfonica` 
-CHANGE COLUMN `data_criacao` `data_composta` YEAR;
+ALTER TABLE `mydb`.`sinfonia` 
+CHANGE COLUMN `data_composta` `data_criacao` date;
 
+-- Adicionar uma coluna genero_musical à tabela sinfonia
+ALTER TABLE `mydb`.`sinfonia`
+ADD COLUMN `genero_musical` VARCHAR(45) AFTER `compositor`;
 
-
-
-
-
-
-
-
-
-
+-- Remover o índice genero_musical da tabela sinfonia
+ALTER TABLE `mydb`.`sinfonia`
+DROP COLUMN `genero_musical`;
 

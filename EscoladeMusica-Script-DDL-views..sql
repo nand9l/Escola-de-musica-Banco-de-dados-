89a48d0a3c9ad824f4496203e8719852586dd2f5
José Fernando <166651961+nand9l@users.noreply.github.com>
2024-10-18 18:05:46 -0300

    Todas as views (DDL)

--- EscoladeMusica-Script-DDL-views..sql
+++ EscoladeMusica-Script-DDL-views..sql
@@ -10,19 +10,7 @@ JOIN
     mydb.musico m ON s.idsinfonia = m.sinfonia_idsinfonia;
 
 
--- 2. View para orquestras e o número de sinfonias que tocaram.
-CREATE OR REPLACE VIEW vw_orquestras_sinfonias AS
-SELECT 
-    o.nome AS orquestra, 
-    COUNT(s.idsinfonia) AS total_sinfonias
-FROM 
-    mydb.orquestra o
-LEFT JOIN 
-    mydb.sinfonia s ON o.sinfonia_idsinfonia = s.idsinfonia
-GROUP BY 
-    o.nome;
-
--- 3. View para compositores e suas respectivas sinfonias em ordem alfabética.
+-- 2. View para compositores e suas respectivas sinfonias em ordem alfabética.
 CREATE OR REPLACE VIEW vw_compositores_sinfonias AS
 SELECT 
     s.compositor, 
@@ -32,19 +20,19 @@ FROM
 ORDER BY 
     s.compositor, s.nome;
 
--- 4. View para contar quantos músicos tocam cada função.
+-- 3. View para contar quantos músicos tocam cada função.
 CREATE OR REPLACE VIEW vw_funcoes_musicos AS
 SELECT 
-    f.descricao AS funcao, 
+    f.descrição AS funcao, 
     COUNT(m.cpf) AS total_musicos
 FROM 
     mydb.funcao f
 LEFT JOIN 
     mydb.musico m ON f.idfuncao = m.funcao_idfuncao
 GROUP BY 
-    f.descricao;
+    f.descrição;
 
--- 5. View para listar sinfonias criadas entre 2021 e 2023.
+-- 4. View para listar sinfonias criadas entre 2021 e 2023.
 CREATE OR REPLACE VIEW vw_sinfonias_2021_2023 AS
 SELECT 
     nome, 
@@ -54,7 +42,7 @@ FROM
 WHERE 
     data_criacao BETWEEN '2021-01-01' AND '2023-12-31';
 
--- 6. View para a sinfonia mais antiga.
+-- 5. View para a sinfonia mais antiga.
 CREATE OR REPLACE VIEW vw_sinfonia_mais_antiga AS
 SELECT 
     nome, 
@@ -62,7 +50,7 @@ SELECT
 FROM 
     mydb.sinfonia;
 
--- 7. View para músicos com suas nacionalidades e as orquestras em que tocam.
+-- 6. View para músicos com suas nacionalidades e as orquestras em que tocam.
 CREATE OR REPLACE VIEW vw_musicos_orquestras AS
 SELECT 
     m.nome AS musico, 
@@ -73,18 +61,18 @@ FROM
 JOIN 
     mydb.orquestra o ON m.orquestra_idorquestra = o.idorquestra;
 
--- 8. View para listar os instrumentos que não têm músicos associados.
+-- 7. View para listar os instrumentos que não têm músicos associados.
 CREATE OR REPLACE VIEW vw_instrumentos_sem_musicos AS
 SELECT 
     i.nome AS instrumento
 FROM 
-    mydb.instrumentos i
+    mydb.instrumento i
 LEFT JOIN 
-    mydb.musico m ON i.idinstrumentos = m.instrumentos_idinstrumentos
+    mydb.musico m ON i.idinstrumento = m.instrumento_idinstrumento
 WHERE 
     m.cpf IS NULL;
 
--- 9. View para contagem de músicos nascidos em cada ano.
+-- 8. View para contagem de músicos nascidos em cada ano.
 CREATE OR REPLACE VIEW vw_musicos_por_ano_nascimento AS
 SELECT 
     YEAR(data_nasc) AS ano_nascimento, 
@@ -94,17 +82,5 @@ FROM
 GROUP BY 
     ano_nascimento;
 
--- 10. View para orquestras com suas datas de criação e o número total de músicos.
-CREATEvw_orquestras_musicosvw_musicos_por_ano_nascimento OR REPLACE VIEW vw_orquestras_musicos AS
-SELECT 
-    o.nome AS orquestra, 
-    o.data_criacao, 
-    COUNT(m.cpf) AS total_musicos
-FROM 
-    mydb.orquestra o
-LEFT JOIN 
-    mydb.musico m ON o.idorquestra = m.orquestra_idorquestra
-GROUP BY 
-    o.nome, o.data_criacao;
     
     
